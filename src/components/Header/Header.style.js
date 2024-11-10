10c1dbcd8d55adbd462050efdff7daf4b2defafb
Wang HoEun <hoeunwang@gmail.com>
2024-11-10 18:44:02 +0900

    feat: header 생성(무피 로고 안 보이는거 수정 필요)

--- src/components/Header/Header.style.js
+++ src/components/Header/Header.style.js
@@ -5,10 +5,31 @@ ${({ theme: { mixin } }) => mixin.flexBox({ justify: 'space-around', align: 'cen
 width:100%;
 height:11rem;
 background-color: white;
-border: 1px solid black;
+padding: 0 6rem;
+box-shadow: 0 2px 5px gray;
+`;
+
+export const NavWrapper = styled.div`
+${({ theme: { mixin } }) => mixin.flexBox({ align: 'center'})};
+height:11rem;
+width:fit-content;
+gap: 8.4rem;
 `;
 
 export const NavBtn = styled.div`
-width: 15rem;
-${({ theme }) => theme.fonts.r_20}
+${({ theme }) => theme.fonts.r_20};
+color: ${({ theme }) => theme.colors.black};
+`;
+export const NavRedBtn = styled.div`
+${({ theme }) => theme.fonts.r_20};
+color: ${({ theme }) => theme.colors.dark_red};
+`;
+
+export const LoginPageBtn = styled.div`
+${({ theme: { mixin } }) => mixin.flexBox({ align: 'center'})};
+gap: 1.4rem;
+width: 19.8rem;
+height: 4.4rem;
+${({ theme }) => theme.fonts.b_20};
+color: ${({ theme }) => theme.colors.gray};
 `;
