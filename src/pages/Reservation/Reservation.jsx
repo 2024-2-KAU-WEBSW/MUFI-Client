9cb85db860e40c1252e28ad1a6f359d7a63cc474
sumhillj <sumhillj@gmail.com>
2024-11-13 20:15:58 +0900

    merge용 commit입니다

--- src/pages/Reservation/Reservation.jsx
+++ src/pages/Reservation/Reservation.jsx
@@ -2,7 +2,9 @@ import * as S from './Reservation.style';
 
 function Reservation () {
     return (
-        <S.ReservationWrapper>예약 페이지 입니다.</S.ReservationWrapper>
+        <S.ReservationWrapper>
+          
+        </S.ReservationWrapper>
     )
 }
 export default Reservation;
--- src/pages/Reservation/Reservation.style.js
+++ src/pages/Reservation/Reservation.style.js
@@ -1,9 +1,9 @@
 import styled from "styled-components";
 
 export const ReservationWrapper = styled.div`
-${({ theme: { mixin } }) => mixin.flexCenter({})};
-${({ theme }) => theme.fonts.b_55_60}
-width:100%;
-height:100vh;
-background-color: ${({ theme }) => theme.colors.white};;
+    ${({ theme: { mixin } }) => mixin.flexCenter({})};
+    ${({ theme }) => theme.fonts.b_55_60}
+    width:100%;
+    height: calc(100vh - 11rem);
+    background-color: ${({ theme }) => theme.colors.white};;  
 `;
